f17a79901a521618d54131e84f0e57b510688d16
Erick Johnson <erick@junctionnetworks.com>
2012-01-19 16:57:56 -0500

    problem implementation

--- application.js
+++ application.js
@@ -1,15 +1,49 @@
-Application = Object.create({
+Application = Object.extend({
+  N_LINES: null,
+
   data: null,
   lines: null,
+  artists: null,
 
   didReceiveData: function(response,text,xml,xhr) {
     Log.info("received data, length:",response.length);
-    this.init(response);
+    this.run(response);
   },
 
-  init: function(response) {
+  run: function(response) {
     this.data = response;
     this.lines = this.data.split("\n");
+    this.artists = {};
+
+    // parse lines and artists
+    var lineArtists;
+    for (var i=0,l=this.lines.length;i<l;i++) {
+      lineArtists = this._getArtistsFromLine(this.lines[i]);
+      for (var j=0,lj=lineArtists.length;j<lj;j++) {
+        lineArtists[j].appendLine(this.lines[i]);
+      }
+    }
+
+    // print
+  },
+
+  _getArtistsFromLine: function(line) {
+    var artistNames = line.split(",");
+
+    var crntArtist,crntName, lineArtists = [];
+    for (var i=0,l=artistNames.length;i<l;i++) {
+      crntName = artistNames[i];
+
+      crntArtist = this.artists[crntName];
+      if (!crntArtist) {
+        crntArtist = Artist.create({name: crntName});
+        crntArtist.init();
+        this.artists[crntName] = crntArtist;
+      }
+
+      lineArtists.push(crntArtist);
+    }
+    return lineArtists;
   }
 });
 
--- artist.js
+++ artist.js
@@ -1,1 +1,16 @@
-Artist = Object.extend({})
+Artist = Object.extend({
+  name: null,
+  lines: null,
+
+  init: function() {
+    this.lines = [];
+  },
+
+  appendLine: function(line) {
+    this.lines.push(line);
+  },
+
+  numLines: function() {
+    return this.lines.length;
+  }
+});
